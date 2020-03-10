bea43918d426510dc8e5695aaeb0749d008aef82
TAMBASCIA Carmine <Carmine.TAMBASCIA@akka.eu>
2020-03-10 15:48:33 +0100

    a dull commit to check connection

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -7,6 +7,7 @@ export default class TodoInput extends Component {
             <div className="card card-body my-3">
                 <form onSubmit={handleSubmit}>
                     <div className="input-group">
+                        I am a test
                             <div className="input-group-prepend">
                                <div className="input-group-text bg-primary text-white">
                                     <i className="fas fa-book"></i>
